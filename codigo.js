6145ffeb897fa11b99b9ea86a2a1b02b45e19f1a
YeisonAlexander2001 <110367659+YeisonAlexander2001@users.noreply.github.com>
2022-08-22 13:40:21 -0500

    Avance capitulo 3 nuevo

--- codigo.js
+++ codigo.js
@@ -26,7 +26,7 @@ frutas = ["burro", "vaca", "perro"];
 
 document.write(frutas[2])*/
 //array asociativos
-let pc1 = {
+/**let pc1 = {
     nombre: "PcYeison",
     procesador: "Inteel core I5",
     ram: "4GB",
@@ -41,5 +41,74 @@ let espacion = pc1["espacio"];
 frase = ` el nombre de mio PC es: ${nombre} <br>
 el procesasor es ${procesador} <br>
 `
-document.write(frase);
+document.write(frase);*/
+/** 
+let numero = 0;
 
+while (numero < 6) {
+    numero++;
+    document.write(numero)
+}*/
+/** 
+let numero = 0;
+
+while(numero < 100){
+    numero++;
+    document.write(numero);
+    if(numero==10){
+        break;
+    }
+}*/
+
+//i = 20;
+// tambien se puede inicializar por fuera
+/** 
+for (let i=6; i >=0; i--){
+    document.write(i + "<br>")
+}
+document.write(i)
+*/
+/** 
+for (let i=1; i <=5; i++){
+    if (i == 3){
+        continue;
+    }
+    document.write(i + "<br>");
+}
+document.write(i)
+
+// FOR IN para el indice de los objetos en un array y FOR OF para el valor de esos objetos
+*/
+/*
+let animales = ["gato", "perro","tiranosaurio rex"];
+
+for (animal in animales){
+    document.write(animal + "<br>");
+}
+
+document.write("<br>")
+
+for(animal of animales){
+    document.write(animal +"<br>");
+}
+*/
+// LABEL PERMITE ASOCIAR UN BUCLE A UN NOMBRE; ES UNA SENTENCIA
+
+array1 = ["maria", "josefa", "roberta"];
+array2 = ["pedro", "marcelo", array1, "josefina"];
+
+forRancio:
+for(let array in array 2){
+    if(array ==2){
+
+        for(let array of array1){
+            if (array == "maria"){
+                continue forRancio;
+            }
+            document.write(array + "<br>");
+        }
+            }
+                    else {
+            document.write(array2[array] + "<br>");
+        }
+}
